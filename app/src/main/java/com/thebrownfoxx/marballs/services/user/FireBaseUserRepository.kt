7398174b2f1442941b0c97a09f938b62238f6523
Cov3n4nt <erosval126@gmail.com>
2024-01-11 12:11:30 +0800

    added UserRepository

--- app/src/main/java/com/thebrownfoxx/marballs/services/user/FireBaseUserRepository.kt
+++ app/src/main/java/com/thebrownfoxx/marballs/services/user/FireBaseUserRepository.kt
@@ -2,9 +2,14 @@ package com.thebrownfoxx.marballs.services.user
 
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.auth.User
+import com.thebrownfoxx.marballs.domain.Outcome
 import kotlinx.coroutines.flow.StateFlow
 
 class FireBaseUserRepository(private val firestore: FirebaseFirestore) : UserRepository {
     override val users: StateFlow<List<User>?>
         get() = TODO("Not yet implemented")
+
+    override suspend fun addUser(user: User): Outcome<Unit> {
+        TODO("Not yet implemented")
+    }
 }
--- app/src/main/java/com/thebrownfoxx/marballs/services/user/UserRepository.kt
+++ app/src/main/java/com/thebrownfoxx/marballs/services/user/UserRepository.kt
@@ -1,8 +1,11 @@
 package com.thebrownfoxx.marballs.services.user
 
+import android.os.OutcomeReceiver
 import com.google.firebase.firestore.auth.User
+import com.thebrownfoxx.marballs.domain.Outcome
 import kotlinx.coroutines.flow.StateFlow
 
 interface UserRepository {
     val users: StateFlow<List<User>?>
+    suspend fun addUser(user: User): Outcome<Unit>
 }
